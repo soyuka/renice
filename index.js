0e399565143d5bbe3446c68bd4b1ab7814307676
Antoine Bluchet <soyuka@gmail.com>
2016-05-25 12:24:06 +0200

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+const Spawner = require('promise-spawner')
+const os = require('os')
+
+const spawner = new Spawner()
+
+/*
+* http://superuser.com/questions/620724/changing-windows-process-priority-via-command-line
+* idle: 64
+* 
+* below normal: 16384
+* 
+* normal: 32
+* 
+* above normal: 32768
+* 
+* high priority: 128
+* 
+* real time: 256
+*/
+
+module.exports = function(pid, priority) {
+  let cmd
+
+  if (os.platform() == 'win32') {
+    cmd = `wmic process where processid="${pid}" CALL setpriority ${priority}`
+  } else {
+    cmd = `renice -n ${priority} -p ${pid}`
+  }
+
+  return spawner.sp(cmd, {detached: true})
+}
